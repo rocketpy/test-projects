67c5b702b4f348569d3f05aa759455d82a4f0043
rocketpy <dimitr.ushakov@gmail.com>
2021-04-12 00:04:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from time import sleep
 from bs4 import BeautifulSoup
@@ -11,6 +12,7 @@ driver = webdriver.Chrome()
 def update_csv():
     df = pd.read_csv("file.csv", names=['Doctor', 'Work time', 'Address', 'Rating'])
     df.to_csv("result.csv", index=False)
+    os.remove("file.csv")
 
 
 # get data from a page
@@ -39,10 +41,10 @@ def get_data(drive):
         data.append(new_data)
 
     df = pd.DataFrame(data)
-    df.to_csv('result.csv', mode='a', index=False, encoding='utf-8', header=False)
+    df.to_csv('file.csv', mode='a', index=False, encoding='utf-8', header=False)
 
 
-# got to a website
+# go to a website
 driver.get("https://www.zooplus.de/tierarzt/results")
 sleep(3)
 # get a cookies
@@ -52,18 +54,20 @@ sleep(3)
 driver.find_element_by_xpath('//*[@id="app"]/div/main/div/div[4]/div[1]/section/div[1]/fieldset/div/div[1]/div[1]/label/input').click()
 sleep(2)
 # pages counter
-counter = 3
+counter = 3  # pages
 while counter > 0:
     # scroll down a page
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     sleep(2)
     get_data(driver)
-    # click on nex page
-    driver.find_element_by_css_selector('#search-results-pane-tab1 > div.pagination-group > nav > ul > li:nth-child(9) > a > span').click()
-    sleep(3)
     counter -= 1
+    if counter > 0:
+        # click on next page
+        driver.find_element_by_css_selector('#search-results-pane-tab1 > div.pagination-group > nav > ul > li:nth-child(9) > a > span').click()
+        sleep(3)
+    else:
+        # update a csv file
+        update_csv()
+        # driver quit
+        driver.quit()
 
-# update a csv file
-update_csv()
-# driver quit
-driver.quit()
