fff3a76d31267add64bb005762be0d55cc3720e1
rocketpy <dimitr.ushakov@gmail.com>
2021-04-14 22:46:18 +0300

    Update using_requests.py

--- using_requests.py
+++ using_requests.py
@@ -1,14 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError
 
 
 useragent = 'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US)'
 headers = {'User-Agent': useragent}
 
 
+def write_csv(data):
+    with open('result.csv', 'a') as f:
+        order = ['Doctor', 'Work time', 'Address', 'Rating']
+        writer = csv.DictWriter(f, fieldnames=order)
+        writer.writerow(data)
+
+
 def get_html(url):
     useragent = {'User-Agent': choice(useragents)}
     r = requests.get(url, headers=headers, timeout=3)
+    # response.content
     return r.text  #
 
 
@@ -20,7 +29,17 @@ def get_data(html):
 
 def main():
     url = 'https://www.zooplus.de/tierarzt/results?animal_99=true'
-    print(get_data(get_html(url)))
+    for url in 
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print('Success!')
+    # print(get_data(get_html(url)))
 
 
 if __name__ == '__main__':
