766d404e2de5b37bba2f1fed8d8c2c0c8064d0e4
rocketpy <dimitr.ushakov@gmail.com>
2021-04-17 23:45:59 +0300

    Update using_requests.py

--- using_requests.py
+++ using_requests.py
@@ -21,6 +21,13 @@ def load_cookies(filename):
     
 """
 s = requests.Session()
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'\
+           'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
+           'Accept':'text/html,application/xhtml+xml,application/xml;'\
+           'q=0.9,image/webp,*/*;q=0.8'}
+url = 'https:'
+req = session.get(url, headers=headers)
+
 s.get('https://')
 r = s.get("https://")
 print(r.text)
