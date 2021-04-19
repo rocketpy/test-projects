675502c12d1932e54bcb0352fc0d509a2be28323
rocketpy <dimitr.ushakov@gmail.com>
2021-04-19 15:31:37 +0300

    Update template.py

--- template.py
+++ template.py
@@ -1,1 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
 
+
+class WebParser:
+    def __init__(self, url):
+        self.url = url
+        
+        
+    def get_page(self):
+        try:
+            req = requests.get(self.url)
+        except requests.ConnectionError:
+            pass
+        if req.stsus_code < 400:
+            return req.content
+          
+          
+    def get_data(self, html):
+        html = self.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        
+        
+    def main(self):
+        pass
+        
+        
+if __name__ == '__main__':
+    web_page = WebParse('https://...')
+    web_page.main()
+        
